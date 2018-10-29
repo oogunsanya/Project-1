4fb76b85d45d208bb53e53673723476150a70c6e
Oluwaseun Ogunsanya <oogunsanya@hawk.iit.edu>
2018-10-29 01:50:40 -0700

    included javascript

---
+++ javascript/java.js
@@ -0,0 +1,6 @@
+function SubmitConfirmation(){
+  var fullname = document.getElementById("name");
+  alert("Thank you " + fullname.value + " for signing up! We hope to see you in the bookstore");
+}
+
+		
